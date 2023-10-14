f6947e8ea9e8024ba06c499132d02f6c4e3740dd
ChristianFeldmann <christian.feldmann@gmx.de>
2023-10-14 11:40:52 +0200

    Add the rest of the parameters

--- src/app/FileProber.cpp
+++ src/app/FileProber.cpp
@@ -4,10 +4,12 @@
  * For a copy, see <https://opensource.org/licenses/MIT>.
  */
 
-#include <iostream>
-
 #include <Demuxer.h>
 #include <libHandling/FFmpegLibrariesInterfaceBuilder.h>
+
+#include <cstddef>
+#include <iomanip>
+#include <iostream>
 
 const auto FILE_NAME = std::string("testFile.webm");
 
@@ -43,6 +45,24 @@ std::string to_string(const std::vector<std::string> stringVec)
 
   const auto outString = stream.str();
   return outString.substr(0, outString.length() - 2);
+}
+
+std::string to_string(const ffmpeg::AVRational rational)
+{
+  return std::to_string(rational.num) + "/" + std::to_string(rational.den);
+}
+
+std::string to_string(const ffmpeg::Size size)
+{
+  return std::to_string(size.width) + "x" + std::to_string(size.height);
+}
+
+std::string to_string(const ffmpeg::ByteVector &bytes)
+{
+  std::ostringstream stream;
+  for (const auto &byte : bytes)
+    stream << "0x" << std::hex << std::to_integer<int>(byte) << " ";
+  return stream.str();
 }
 
 void printOutLog(const ffmpeg::Log &log)
@@ -83,28 +103,35 @@ int main(int argc, char const *argv[])
   std::cout << "\nFile info:\n";
 
   std::cout << "  Format:\n";
-  std::cout << "    Name       : " << inputFormat.getName() << "\n";
-  std::cout << "    Long Name  : " << inputFormat.getLongName() << "\n";
-  std::cout << "    Flags      : " << to_string(inputFormat.getFlags()) << "\n";
-  std::cout << "    Extensions : " << inputFormat.getExtensions() << "\n";
-  std::cout << "    Mime Type  : " << inputFormat.getMimeType() << "\n";
-  std::cout << "    Start time : " << formatContext.getStartTime() << "\n";
-  std::cout << "    Duration   : " << formatContext.getDuration() << "\n";
+  std::cout << "    Name              : " << inputFormat.getName() << "\n";
+  std::cout << "    Long Name         : " << inputFormat.getLongName() << "\n";
+  std::cout << "    Flags             : " << to_string(inputFormat.getFlags()) << "\n";
+  std::cout << "    Extensions        : " << inputFormat.getExtensions() << "\n";
+  std::cout << "    Mime Type         : " << inputFormat.getMimeType() << "\n";
+  std::cout << "    Start time        : " << formatContext.getStartTime() << "\n";
+  std::cout << "    Duration          : " << formatContext.getDuration() << "\n";
 
   for (const auto &stream : formatContext.getStreams())
   {
     std::cout << "  Streams " << stream.getIndex() << ":\n";
-    std::cout << "    Codec Type      : " << to_string(stream.getCodecType()) << "\n";
-    std::cout << "    Codec Type Name : " << stream.getCodecTypeName() << "\n";
+    std::cout << "    Codec Type        : " << to_string(stream.getCodecType()) << "\n";
+    std::cout << "    Codec Type Name   : " << stream.getCodecTypeName() << "\n";
 
-    std::cout << "    Codec Description:\n";
+    std::cout << "    Codec Description\n";
     const auto codecDescriptor = stream.getCodecDescriptor();
-    std::cout << "      Media Type : " << to_string(codecDescriptor.getMediaType()) << "\n";
-    std::cout << "      Name       : " << codecDescriptor.getCodecName() << "\n";
-    std::cout << "      Long Name  : " << codecDescriptor.getLongName() << "\n";
-    std::cout << "      Properties : " << to_string(codecDescriptor.getProperties()) << "\n";
-    std::cout << "      Mime Types : " << to_string(codecDescriptor.getMimeTypes()) << "\n";
-    std::cout << "      Profiles   : " << to_string(codecDescriptor.getProfiles()) << "\n";
+    std::cout << "      Media Type      : " << to_string(codecDescriptor.getMediaType()) << "\n";
+    std::cout << "      Name            : " << codecDescriptor.getCodecName() << "\n";
+    std::cout << "      Long Name       : " << codecDescriptor.getLongName() << "\n";
+    std::cout << "      Properties      : " << to_string(codecDescriptor.getProperties()) << "\n";
+    std::cout << "      Mime Types      : " << to_string(codecDescriptor.getMimeTypes()) << "\n";
+    std::cout << "      Profiles        : " << to_string(codecDescriptor.getProfiles()) << "\n";
+
+    std::cout << "    Average Framerate : " << to_string(stream.getAverageFrameRate()) << "\n";
+    std::cout << "    Time Base         : " << to_string(stream.getTimeBase()) << "\n";
+    std::cout << "    Frame Size        : " << to_string(stream.getFrameSize()) << "\n";
+    std::cout << "    Colorspace        : " << to_string(stream.getColorspace()) << "\n";
+    std::cout << "    Pixel Format      : " << to_string(stream.getPixelFormat()) << "\n";
+    std::cout << "    Extradata         : " << to_string(stream.getExtradata()) << "\n";
   }
 
   return 0;
