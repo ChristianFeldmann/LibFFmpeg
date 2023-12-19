bd730c20ffa378c1c4a8d23b42f3c31dd61341c0
ChristianFeldmann <christian.feldmann@gmx.de>
2023-12-19 09:43:03 +0100

    Added 2 more missing includes

--- src/app/FileProber.cpp
+++ src/app/FileProber.cpp
@@ -11,6 +11,7 @@
 #include <filesystem>
 #include <iomanip>
 #include <iostream>
+#include <sstream>
 
 using namespace ffmpeg;
 
--- src/lib/common/Formatting.cpp
+++ src/lib/common/Formatting.cpp
@@ -8,6 +8,7 @@
 
 #include <iomanip>
 #include <iostream>
+#include <sstream>
 
 namespace ffmpeg
 {
