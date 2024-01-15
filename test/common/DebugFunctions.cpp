3daac70e382b00eb1ea7838208a442445ad0fab1
ChristianFeldmann <christian.feldmann@gmx.de>
2024-01-15 09:10:17 +0100

    Add a function that can write out YUV frames. Very useful for testing.

---
+++ test/common/DebugFunctions.cpp
@@ -0,0 +1,28 @@
+/* Copyright (c) 2023 Christian Feldmann [christian.feldmann@gmx.de].
+ * All rights reserved.
+ * This work is licensed under the terms of the MIT license.
+ * For a copy, see <https://opensource.org/licenses/MIT>.
+ */
+
+#include "DebugFunctions.h"
+
+namespace ffmpeg::test
+{
+
+FrameFileWriter::FrameFileWriter(std::string filename)
+{
+  this->outfile.open(filename, std::ios::binary | std::ios::out);
+}
+
+void FrameFileWriter::write(const avutil::AVFrameWrapper &frame)
+{
+  const auto pixelFormat = frame.getPixelFormatDescriptor();
+
+  for (int component = 0; component < pixelFormat.numberOfComponents; ++component)
+  {
+    const auto data = frame.getData(component);
+    this->outfile.write(reinterpret_cast<const char *>(data.data()), data.size());
+  }
+}
+
+} // namespace ffmpeg::test
